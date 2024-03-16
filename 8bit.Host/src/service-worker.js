04d3cd9a868f51b5ff634ed96c55f8448372d3d7
MrAntix <aj@antix.co.uk>
2024-03-16 12:49:00 +0000

    fix(sw): update strategy

--- 8bit.Host/src/service-worker.js
+++ 8bit.Host/src/service-worker.js
@@ -1,21 +1,4 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = '[[BuildVersion]]';
-const RUNTIME = 'runtime';
+const VERSION = '[[BuildVersion]]';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -25,19 +8,17 @@ const PRECACHE_URLS = [
     'index.html',
     './', // Alias for index.html
     'main.js',
-    'manifest.json',
     'sanitize.js'
 ];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(PRECACHE)
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(VERSION)
             .then(cache => {
                 return Promise.all(
-                    PRECACHE_URLS.map(asset => {
-                        return cache.add(asset).catch(error => {
-                            console.error(`Caching failed for ${asset}:`, error);
+                    PRECACHE_URLS.map(item => {
+                        return cache.add(item).catch(error => {
+                            console.error(`Error caching ${item}:`, error);
                         });
                     })
                 );
@@ -46,12 +27,13 @@ self.addEventListener('install', event => {
     );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+self.addEventListener('activate', e => {
+    const currentCaches = [VERSION];
+
+    e.waitUntil(
+        caches.keys().then(names => {
+            return names.filter(name => !currentCaches.includes(name));
+
         }).then(cachesToDelete => {
             return Promise.all(cachesToDelete.map(cacheToDelete => {
                 return caches.delete(cacheToDelete);
@@ -60,27 +42,24 @@ self.addEventListener('activate', event => {
     );
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-    // Skip cross-origin requests, like those for Google Analytics.
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+self.addEventListener('fetch', e => {
 
-                return caches.open(RUNTIME).then(cache => {
-                    return fetch(event.request).then(response => {
-                        // Put a copy of the response in the runtime cache.
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
-                    });
-                });
-            })
+    if (e.request.url.startsWith(self.location.origin)) {
+
+        e.respondWith(
+            getCacheFirst(e.request)
         );
     }
 });
+
+async function getCacheFirst(request) {
+    let response = await caches.match(request);
+
+    if (response == null) {
+        const cache = await caches.open(VERSION);
+        const response = await fetch(request);
+        await cache.put(request, response.clone());
+    }
+
+    return response;
+}
